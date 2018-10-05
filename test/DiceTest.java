4ef0aa68aa2f90078969962fd08ab0fd7fc66b40
Emil Johannes Lundqvist <emlunde@gmail.com>
2018-10-05 11:43:11 +0200

    Added counters

--- test/DiceTest.java
+++ test/DiceTest.java
@@ -10,9 +10,14 @@ class DiceTest {
     void testRoll(){
         Dice dice = new Dice();
 
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
+
+
+
 
         for(int i = 0; i < 60000; i++){
             int num = dice.roll();
+
             assertTrue(num>0 && num < 7);
 
         }
