2fd0e6b2f1e01e89dceee69bdc6181635ced397c
Armandas <armandas.rokas@gmail.com>
2018-10-05 11:33:43 +0200

    Test fil

--- test/DiceTest.java
+++ test/DiceTest.java
@@ -1,10 +1,23 @@
 import org.junit.jupiter.api.Test;
+import terning.Dice;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class DiceTest {
 
+    @Test
+    void testRoll(){
+        Dice dice = new Dice();
+
+
+        for(int i = 0; i < 60000; i++){
+            int num = dice.roll();
+            assertTrue(num>0 && num < 7);
+
+        }
+
+    }
 
 
 }
